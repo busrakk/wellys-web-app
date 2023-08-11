aa452851a0f2bf6a7d27610014913780737512e8
busrakk <busrakartal160@gmail.com>
2023-08-11 15:31:10 +0300

    add product creation

---
+++ client/src/components/admin/product/Form.js
@@ -0,0 +1,189 @@
+import React from "react";
+import { Select } from "antd";
+const { Option } = Select;
+
+const Form = (props) => {
+  return (
+    <div className="fixed z-0 inset-x-0 top-14 bottom-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
+      <div className="p-2 rounded">
+        <div className="flex justify-center">
+          <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5 w-3/4">
+            {props.isLoading === true ? (
+              <div className="bg-white px-6 py-4">Loading</div>
+            ) : (
+              <form
+                onSubmit={props.handleSubmit}
+                encType="multipart/form-data"
+                className="bg-white px-6 py-4 overflow-auto"
+              >
+                <div className="flex flex-col mb-4">
+                  <label
+                    htmlFor="category"
+                    className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                  >
+                    Ürün Kategorisi:
+                  </label>
+                  <Select
+                    bordered={false}
+                    placeholder="Select a category"
+                    size="large"
+                    showSearch
+                    onChange={(value) => {
+                      props.setCategory(value);
+                    }}
+                    className="text-fontxs sm:text-fontsm placeholder-gray-500 rounded-lg w-full py-2 focus:outline-none focus:border-indigo-400"
+                  >
+                    {props.categories?.map((item) => (
+                      <Option key={item.id} value={item?._id}>
+                        {item?.name}
+                      </Option>
+                    ))}
+                  </Select>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 space-x-4">
+                  <div className="flex flex-col mb-4">
+                    <label
+                      htmlFor="name"
+                      className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                    >
+                      Ürün Adı:
+                    </label>
+                    <input
+                      id="name"
+                      type="text"
+                      className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                      placeholder="Ürün Adı"
+                      onChange={(e) => props.setName(e.target.value)}
+                      value={props.name}
+                      name="name"
+                      required
+                    />
+                  </div>
+                  <div className="flex flex-col mb-4">
+                    <label
+                      htmlFor="price"
+                      className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                    >
+                      Ürün Fiyatı:
+                    </label>
+                    <input
+                      id="price"
+                      type="number"
+                      className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                      placeholder="Ürün Fiyatı"
+                      onChange={(e) => props.setPrice(e.target.value)}
+                      value={props.price}
+                      name="price"
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col mb-4">
+                  <label
+                    htmlFor="description"
+                    className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                  >
+                    Ürün Tanımı:
+                  </label>
+                  <textarea
+                    id="description"
+                    type="text"
+                    className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                    placeholder="Ürün Tanımı"
+                    onChange={(e) => props.setDescription(e.target.value)}
+                    value={props.description}
+                    name="description"
+                  ></textarea>
+                </div>
+                <div className="flex flex-col mb-4">
+                  <label
+                    htmlFor="upload images"
+                    className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                  >
+                    {props.photo ? props.photo.name : "Upload Photo :"}
+                  </label>
+                  <input
+                    type="file"
+                    className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                    placeholder="Ürün Resmi"
+                    value={props.image}
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => props.setPhoto(e.target.files[0])}
+                    //hidden
+                  />
+                </div>
+                <div className="flex flex-col mb-4">
+                  {props.photo && (
+                    <div className="flex justify-center items-center">
+                      <img
+                        src={URL.createObjectURL(props.photo)}
+                        alt="product_photo"
+                        height={"200px"}
+                      />
+                    </div>
+                  )}
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 space-x-4">
+                  <div className="flex flex-col mb-4">
+                    <label
+                      htmlFor="featured"
+                      className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                    >
+                      Öne Çıkarma:
+                    </label>
+                    <select
+                      id="featured"
+                      name="featured"
+                      onChange={(e) => props.setFeatured(e.target.value)}
+                      value={props.featured}
+                      className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                    >
+                      <option value={1}>Active</option>
+                      <option value={0}>Inactive</option>
+                    </select>
+                  </div>
+                  <div className="flex flex-col mb-4">
+                    <label
+                      htmlFor="status"
+                      className="mb-1 text-fontxs sm:text-fontsm tracking-wide text-gray-600"
+                    >
+                      Durum:
+                    </label>
+                    <select
+                      id="status"
+                      name="status"
+                      onChange={(e) => props.setStatus(e.target.value)}
+                      value={props.status}
+                      className="text-fontxs sm:text-fontsm placeholder-gray-500 pl-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-indigo-400"
+                    >
+                      <option value={1}>Active</option>
+                      <option value={0}>Inactive</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={() => props.setShow(false)} // Close the form
+                    className="mr-2 px-4 py-2 text-fontsm font-medium tracking-wide text-gray-600 capitalize transition-colors duration-300 transform bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+                  >
+                    İptal
+                  </button>
+                  <button
+                    type="submit"
+                    className="px-4 py-2 text-fontsm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-violet-600 rounded-lg md:text-fontmd hover:bg-violet-500 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80"
+                  >
+                    {props.button}
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
--- client/src/components/admin/product/index.js
+++ client/src/components/admin/product/index.js
@@ -1,25 +1,113 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineStar } from "react-icons/ai";
 import { MdAddCircleOutline } from "react-icons/md";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Form from "./Form";
 
 const Product = () => {
-  return (
-    <div className="h-full ml-14 mt-20 mb-10 md:ml-64 fixed overflow-x:hidden overflow-y-scroll pb-24">
-      <div className="mt-4 text-fontlg font-semibold flex justify-between md:mx-24 mx-10">
-        <span>Yemekler</span>
-        <button className="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize bg-violet-600 rounded-lg md:text-fontmd text-fontsm hover:bg-violet-500 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80">
-          <MdAddCircleOutline size={28} className="w-5 h-5 mx-1" />
-          <span className="mx-1">Ekle</span>
-        </button>
-      </div>
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [featured, setFeatured] = useState("0");
+  const [status, setStatus] = useState("1");
+  const [isLoading, setIsLoading] = useState(true);
+  const [show, setShow] = useState(false);
 
-      <div className="md:mx-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-6 mb-5 gap-4">
-        <div className=" px-4 pt-5 pb-4 overflow-hidden text-left align-bottom bg-white rounded-lg shadow-xl sm:my-4 sm:align-middle sm:max-w-md sm:w-full sm:p-6">
+  useEffect(() => {
+    getAllProducts();
+    getAllCategories();
+  }, []);
+
+  // get all category
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_ROOT_URL}/api/category/get-category`
+      );
+      if (data.success) {
+        setIsLoading(false);
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  // get all category
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_BACKEND_ROOT_URL}/api/product/get-product`
+      );
+      if (data?.success) {
+        setIsLoading(false);
+        setProducts(data?.products);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  //create product
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const productData = new FormData();
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("photo", photo);
+      productData.append("category", category);
+      productData.append("featured", featured);
+      productData.append("status", status);
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BACKEND_ROOT_URL}/api/product/create-product`,
+        productData
+      );
+      if (data?.success) {
+        setShow(false);
+        toast.success(`${name} is successfully created`);
+        resetForm();
+        getAllProducts();
+      } else {
+        toast.error(data?.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in input form");
+    }
+  };
+
+  const resetForm = () => {
+    setName("");
+    setStatus("");
+    setCategories("");
+    setDescription("");
+    setFeatured("");
+    setPhoto("");
+    setPrice("");
+  };
+
+  const renderTableData = () => {
+    let view = [];
+    products.map((item) => {
+      view.push(
+        <div
+          key={item._id}
+          className=" px-4 pt-5 pb-4 overflow-hidden text-left align-bottom bg-white rounded-lg shadow-xl sm:my-4 sm:align-middle sm:max-w-md sm:w-full sm:p-6"
+        >
           <div>
             <img
               className="object-cover w-full h-48 rounded-md"
-              src="https://picsum.photos/200/300"
-              alt=""
+              src={item?.image}
+              alt={item.name}
             />
 
             <div className="mt-4 text-center">
@@ -27,40 +115,86 @@ const Product = () => {
                 className="font-medium leading-6 text-gray-800 capitalize"
                 id="modal-title"
               >
-                name
+                {item.name}
               </h3>
             </div>
           </div>
 
           <div className="mt-5 sm:flex sm:items-center sm:-mx-2">
             <div
-              //   className={`w-full flex justify-center px-1 py-1 text-sm font-medium tracking-wide  capitalize border rounded-full sm:w-1/2 sm:mx-2  focus:outline-none focus:ring  focus:ring-opacity-40 ${
-              //     item.featured === 1
-              //       ? " border-yellow-500 text-yellow-500"
-              //       : " border-gray-500 text-gray-500"
-              //   } `}
-
-              className={`w-full flex justify-center px-1 py-1 text-sm font-medium tracking-wide  capitalize border rounded-full sm:w-1/2 sm:mx-2  focus:outline-none focus:ring  focus:ring-opacity-40  border-yellow-500 text-yellow-500"
-                  `}
-
-              // className={`w-full flex justify-center px-1 py-1 text-sm font-medium tracking-wide  capitalize border rounded-full sm:w-1/2 sm:mx-2  focus:outline-none focus:ring  focus:ring-opacity-40 ${
-              //   item.featured === 1
-              //     ? "focus:ring-gray-500 hover:text-gray-500 hover:border-gray-500 focus:text-gray-500  border-yellow-500 text-yellow-500"
-              //     : "focus:ring-yellow-500 hover:text-yellow-500 hover:border-yellow-500 focus:text-yellow-500  border-gray-500 text-gray-500"
-              // } `}
+              className={`w-full flex justify-center px-1 py-1 text-sm font-medium tracking-wide  capitalize border rounded-full sm:w-1/2 sm:mx-2  focus:outline-none focus:ring  focus:ring-opacity-40 ${
+                item.featured === "1"
+                  ? " border-yellow-500 text-yellow-500"
+                  : " border-gray-500 text-gray-500"
+              } `}
             >
               <AiOutlineStar size={32} />
             </div>
 
-            <button className="w-full px-2 py-2 mt-4 text-sm font-medium tracking-wide text-white capitalize bg-blue-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40">
+            <button
+              //onClick={() => handleModal(true, item.id)}
+              className="w-full px-2 py-2 mt-4 text-sm font-medium tracking-wide text-white capitalize bg-blue-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+            >
               Düzenle
             </button>
 
-            <button className="w-full px-2 py-2 mt-4 text-sm font-medium tracking-wide text-white capitalize bg-red-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-40">
+            <button
+              //onClick={(e) => handleDelete(e, item.id)}
+              className="w-full px-2 py-2 mt-4 text-sm font-medium tracking-wide text-white capitalize bg-red-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-40"
+            >
               Sil
             </button>
           </div>
         </div>
+      );
+      return view;
+    });
+    if (view.length === 0) {
+      return <div>No data found!</div>;
+    } else {
+      return view;
+    }
+  };
+
+  return (
+    <div className="h-full ml-14 mt-20 mb-10 md:ml-64 fixed overflow-x:hidden overflow-y-scroll pb-24">
+      <div className="mt-4 text-fontlg font-semibold flex justify-between md:mx-24 mx-10">
+        <span>Yemekler</span>
+        <button
+          onClick={() => setShow(true)}
+          className="flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize bg-violet-600 rounded-lg md:text-fontmd text-fontsm hover:bg-violet-500 focus:outline-none focus:ring focus:ring-violet-300 focus:ring-opacity-80"
+        >
+          <MdAddCircleOutline size={28} className="w-5 h-5 mx-1" />
+          <span className="mx-1">Ekle</span>
+        </button>
+      </div>
+
+      {show && (
+        <Form
+          setShow={setShow}
+          categories={categories}
+          setCategory={setCategory}
+          photo={photo}
+          setPhoto={setPhoto}
+          name={name}
+          setName={setName}
+          description={description}
+          setDescription={setDescription}
+          price={price}
+          setPrice={setPrice}
+          status={status}
+          setStatus={setStatus}
+          featured={featured}
+          setFeatured={setFeatured}
+          isLoading={isLoading}
+          handleSubmit={handleSubmit}
+          button="Ekle"
+        />
+      )}
+
+      <div className="md:mx-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-6 mb-5 gap-4">
+        {isLoading && <div>Loading</div>}
+        {!isLoading && renderTableData()}
       </div>
     </div>
   );
